37b4d68ef7ba597a6393f247f690c7dce5a2e1dc
Sruthi Kannan <skannan6@ncsu.edu>
2024-09-03 15:44:38 -0400

    Finished Challenge Question 00!

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,17 @@
+"""My first challenge question in COMP110!"""
+
+__author__ = "730536653"
+
+
+def mimic(message: str) -> str:
+    """Defining a mimic function to return the input message back to me"""
+    return message
+
+
+def main() -> None:
+    """This function combines all the functions into a main function"""
+    return print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
